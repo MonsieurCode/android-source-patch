4adc2f46bcebd57854e9122304536b49ba5888f6
Darren Pottinger <darren.pottinger@gmail.com>
2015-12-19 13:32:46 -0500

    Update evdev.c

--- udoo/4.4.2/kernel_imx/drivers/input/evdev.c
+++ udoo/4.4.2/kernel_imx/drivers/input/evdev.c
@@ -1041,15 +1041,26 @@ static const struct input_device_id evdev_ids[] = {
 
 MODULE_DEVICE_TABLE(input, evdev_ids);
 
+bool static evdev_match(struct input_handler *handler, struct input_dev *dev) 
+{
+	/* Avoid EETI USB touchscreens */
+	#define VID_EETI 0x0EEF
+	if ((BUS_USB == dev->id.bustype)&&(VID_EETI==dev->id.vendor)) {
+		return false;
+	}
+	
+	return true;
+}
+
 static struct input_handler evdev_handler = {
 	.event		= evdev_event,
+	.match		= evdev_match, /* Added by EETI */
 	.connect	= evdev_connect,
 	.disconnect	= evdev_disconnect,
 	.fops		= &evdev_fops,
 	.minor		= EVDEV_MINOR_BASE,
 	.name		= "evdev",
 	.id_table	= evdev_ids,
-//	.blacklist	= evdev_blacklist, /* Added by EETI */
 };
 
 static int __init evdev_init(void)
