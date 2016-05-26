3841c9268d58929da351aab5227fb6a83b000dfe
Monsieur LLC <monsieur@monsieur.co>
2016-05-26 00:03:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 wget https://drive.google.com/open?id=0B9oMfppL-n1zR1FrZkRMak90a1U -O ~/Documents/udoo-quad-kitkat.tar.gz
 tar zxvf ~/Documents/udoo-quad-kitkat.tar.gz
 mv ~/Documents/4.4.2 ~/Documents/udoo-quad-kitkat
+cd ~/Documents/udoo-quad-kit
+. setup udoo-eng
+make -C kernel_imx imx6_udoo_android_defconfig
+make
+bootable/bootloader/uboot-imx/compile.sh
+sudo -E ./make_sd.sh /dev/sdb
