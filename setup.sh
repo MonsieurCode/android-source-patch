c49f1db2a930fe64610d214dca51b9d74b6fb09a
Monsieur LLC <monsieur@monsieur.co>
2016-05-26 00:03:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-
+wget https://drive.google.com/open?id=0B9oMfppL-n1zR1FrZkRMak90a1U -O ~/Documents/udoo-quad-kitkat.tar.gz
+tar zxvf ~/Documents/udoo-quad-kitkat.tar.gz
+mv ~/Documents/4.4.2 ~/Documents/udoo-quad-kitkat
